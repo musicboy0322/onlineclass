eafe8b413371212b8bab832b6b5d7ba6fcc84bda
Kyle <plusultra0322@gmail.com>
2021-06-01 15:03:37 +0800

    2021/6/1 in class

---
+++ src/main/java/car/CarWin.java
@@ -0,0 +1,24 @@
+package car;
+
+import javax.swing.*;
+
+public class CarWin {
+
+    static int width = 500;
+    static int height = 400;
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        frame.setVisible(true);
+        frame.setSize(width, height);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+
+
+
+    }
+
+
+
+}
